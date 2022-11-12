46c39dd55c308fbcd3728b6b6758a57d8d1b6d1d
carllytli <carllytli@tencent.com>
2022-11-12 16:58:31 +0800

    feat: 测试reoder 实例,兼容x86

--- example/reorder_example.cc
+++ example/reorder_example.cc
@@ -3,6 +3,67 @@
 #include <semaphore.h>
 #include <thread>
 
+// Set either of these to 1 to prevent CPU reordering
+#define USE_CPU_FENCE 0
+#define USE_SINGLE_HW_THREAD 0 // Supported on Linux, but not Cygwin or PS3
+
+#if USE_SINGLE_HW_THREAD
+#include <sched.h>
+#endif
+
+//-------------------------------------
+//  MersenneTwister
+//  A thread-safe random number generator with good randomness
+//  in a small number of instructions. We'll use it to introduce
+//  random timing delays.
+//-------------------------------------
+#define MT_IA 397
+#define MT_LEN 624
+
+class MersenneTwister {
+  unsigned int m_buffer[MT_LEN];
+  int m_index;
+
+public:
+  MersenneTwister(unsigned int seed);
+  // Declare noinline so that the function call acts as a compiler barrier:
+  unsigned int integer() __attribute__((noinline));
+};
+
+MersenneTwister::MersenneTwister(unsigned int seed) {
+  // Initialize by filling with the seed, then iterating
+  // the algorithm a bunch of times to shuffle things up.
+  for (int i = 0; i < MT_LEN; i++)
+    m_buffer[i] = seed;
+  m_index = 0;
+  for (int i = 0; i < MT_LEN * 100; i++)
+    integer();
+}
+
+unsigned int MersenneTwister::integer() {
+  // Indices
+  int i = m_index;
+  int i2 = m_index + 1;
+  if (i2 >= MT_LEN)
+    i2 = 0; // wrap-around
+  int j = m_index + MT_IA;
+  if (j >= MT_LEN)
+    j -= MT_LEN; // wrap-around
+
+  // Twist
+  unsigned int s = (m_buffer[i] & 0x80000000) | (m_buffer[i2] & 0x7fffffff);
+  unsigned int r = m_buffer[j] ^ (s >> 1) ^ ((s & 1) * 0x9908B0DF);
+  m_buffer[m_index] = r;
+  m_index = i2;
+
+  // Swizzle
+  r ^= (r >> 11);
+  r ^= (r << 7) & 0x9d2c5680UL;
+  r ^= (r << 15) & 0xefc60000UL;
+  r ^= (r >> 18);
+  return r;
+}
+
 sem_t sem1, sem2;
 sem_t end1, end2;
 int x, y;
@@ -10,9 +71,10 @@ int r1, r2;
 int threshold = 0;
 
 void thread_worker1() {
+  MersenneTwister random(1);
   for (;;) {
     sem_wait(&sem1);
-    while (rand() % 9 != 0)
+    while (random.integer() % 8 != 0)
       ;
     y = 1;
     asm volatile("" ::: "memory");
@@ -24,10 +86,11 @@ void thread_worker1() {
 }
 
 void thread_worker2() {
+  MersenneTwister random(1);
   for (;;) {
 
     sem_wait(&sem2);
-    while (rand() % 9 != 0)
+    while (random.integer() % 8 != 0)
       ;
     x = 1;
     asm volatile("mfence" ::: "memory");
